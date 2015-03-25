f236fecaa5fe4bd66cd56e514bd99a81a6075ee5
MashaTelyatnikova <mashatelatnikova@gmail.com>
2015-03-25 21:10:57 +0500

    Добавлена задача на инвертированный индекс и тесты к ней.

---
+++ OOP.Implementation/InvertedIndexUtils/InvertedIndexBuilder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace OOP.Implementation.InvertedIndexUtils
+{
+    public static class InvertedIndexBuilder
+    {
+        public static Dictionary<string, List<int>> BuildInvertedIndex(Document[] documents)
+        {
+            var result = new Dictionary<string, List<int>>();
+
+            foreach (var document in documents)
+            {
+                var documentTextWords = Regex.Split(document.Text.ToLower(), @"\W+")
+                                                .Where(word => word != "")
+                                                .Distinct();
+
+                foreach (var word in documentTextWords)
+                {
+                    if (!result.ContainsKey(word))
+                        result[word] = new List<int>();
+
+                    result[word].Add(document.Id);
+                }
+            }
+
+            return result;
+        }
+
+        public static Dictionary<string, List<int>> BuildInvertedIndexSecondVersion(Document[] documents)
+        {
+            return documents.SelectMany(document => Regex.Split(document.Text.ToLower(), @"\W+")
+                                                            .Where(word => word != "")
+                                                            .Distinct()
+                                                            .Select(ii => Tuple.Create(ii, document.Id))
+                                       )
+                                .GroupBy(i => i.Item1)
+                                .ToDictionary(i => i.Key, i => i.Select(g => g.Item2).ToList());
+        } 
+    }
+}
