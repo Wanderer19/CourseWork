8f92484ce8760da00177295c2fc615f16adfad83
MashaTelyatnikova <mashatelatnikova@gmail.com>
2015-03-25 15:38:01 +0500

    Добавлена задача про частотный словарь и тесты к ней.

---
+++ OOP.Implementation/FrequencyDictionary.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OOP.Implementation
+{
+    public class FrequencyDictionary
+    {
+
+        public static Dictionary<string, int> GetFrequenciesAtText(string text)
+        {
+            var words =
+                text.Split(new char[] {' ', ',', '.', '!', '?'}, StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            var frequencies = new Dictionary<string, int>();
+            foreach (var word in words)
+            {
+                if (!frequencies.ContainsKey(word))
+                    frequencies[word] = 0;
+
+                frequencies[word]++;
+            }
+
+            return frequencies;
+        }
+
+        public static Dictionary<string, int> GetFrequenciesAtTextSecondVersion(string text)
+        {
+
+            return
+                text.Split(new char[] {' ', ',', '.', '!', '?'}, StringSplitOptions.RemoveEmptyEntries)
+                    .GroupBy(word => word)
+                    .ToDictionary(key => key.Key, key => key.Count());
+        }
+    }
+}
