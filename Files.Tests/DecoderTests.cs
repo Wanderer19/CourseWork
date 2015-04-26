aa5af710cc616b9d36f12425d7996f1f3e3d8325
MashaTelyatnikova <mashatelatnikova@gmail.com>
2015-04-26 23:23:58 +0500

    Тесты.

--- Files.Tests/DecoderTests.cs
+++ Files.Tests/DecoderTests.cs
@@ -9,6 +9,7 @@ namespace Files.Tests
     {
         [TestCase("DecoderTests\\67.result", "Yo", "Nice", "it is my life it is now or never i aint gonna live forever i just wanna live while i am alive it is my life ")]
         [TestCase("DecoderTests\\test1.result", "masha", "end", "hello")]
+        [TestCase("DecoderTests\\test2.result", "masha", "end", "")]
         public void Test(string cipherFile, string letterId, string wordEndId, string expectedResult)
         {
             var decoder = new Decoder();
--- Files.Tests/ProblemDirectorySolverTests.cs
+++ Files.Tests/ProblemDirectorySolverTests.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Files.Solutions;
 using NUnit.Framework;
 
 namespace Files.Tests
@@ -10,5 +12,22 @@ namespace Files.Tests
     [TestFixture]
     public class ProblemDirectorySolverTests
     {
+        [Test]
+        public void Test()
+        {
+            var solver = new ProblemDirectorySolver();
+
+            solver.Solve("DirectoryTests\\input", "DirectoryTests\\output");
+
+            var info = new DirectoryInfo("DirectoryTests\\input");
+            foreach (var file in info.EnumerateFiles())
+            {
+                var actual = File.ReadAllText("DirectoryTests\\output\\" + file.Name).Trim();
+                var expected = File.ReadAllText("DirectoryTests\\results\\" + file.Name).Trim();
+
+                Assert.That(actual, Is.EqualTo(expected));
+            }
+
+        }
     }
 }
