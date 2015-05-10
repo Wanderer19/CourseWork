0096fdbc1095f0157f62142e7bf534b976c7fdb0
MashaTelyatnikova <mashatelatnikova@gmail.com>
2015-05-11 01:48:35 +0500

    добавлено описание к задаче.

--- OOP.Solutions/LibraryUtils/Magazine.cs
+++ OOP.Solutions/LibraryUtils/Magazine.cs
@@ -7,25 +7,25 @@ namespace OOP.Solutions.LibraryUtils
     {
         public DateTime Release { get; set; }
         public string Name { get; set; }
-        private readonly List<Article> articles;
+        public List<Article> Articles;
  
         public Magazine(string name, DateTime release)
         {
             Release = release;
             Name = name;
-            articles = new List<Article>();
+            Articles = new List<Article>();
         }
 
         public Magazine(string name, DateTime release, List<Article> articles)
         {
             Release = release;
             Name = name;
-            this.articles = articles;
+            this.Articles = articles;
         }
 
         public void AddArticle(Article article)
         {
-            articles.Add(article);
+            Articles.Add(article);
         }
     }
 }
